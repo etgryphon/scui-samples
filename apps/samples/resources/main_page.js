9f2997a57651e056e837f413306372f13d99b042
Evin Grano <etgryphon@cox.net>
2009-10-06 20:45:41 -0400

    initial layout of the test code...

--- apps/samples/resources/main_page.js
+++ apps/samples/resources/main_page.js
@@ -11,13 +11,58 @@ Samples.mainPage = SC.Page.design({
   // Add childViews to this pane for views to display immediately on page 
   // load.
   mainPane: SC.MainPane.design({
-    childViews: 'labelView'.w(),
+    childViews: 'sampleTabView'.w(),
     
-    labelView: SC.LabelView.design({
-      layout: { centerX: 0, centerY: 0, width: 200, height: 18 },
-      textAlign: SC.ALIGN_CENTER,
-      tagName: "h1", 
-      value: "Welcome to SproutCore!"
+    sampleTabView: SC.TabView.design({
+  		layout: { top: 30, left: 30, right: 30, bottom: 30 },
+      classNames: ['sample-tabs'],
+      nowShowing: 'Samples.mainPage.calendarSampleView', 
+      items: [
+        { title: "Calendar".loc(), value:'Samples.mainPage.calendarSampleView' },
+        { title: "Auto-Suggest".loc(), value:'Samples.mainPage.autoSuggestSampleView' },
+        { title: "Collapsible".loc(), value:'Samples.mainPage.collapsibleSampleView' },
+        { title: "Context Menu Pane".loc(), value:'Samples.mainPage.contextMenuSampleView' },
+        { title: "ContentEditable".loc(), value:'Samples.mainPage.contentEditableSampleView' },
+        { title: "Upload".loc(), value:'Samples.mainPage.uploadSampleView' },
+        { title: "Mixins".loc(), value:'Samples.mainPage.mixinSampleView' }
+      ],
+      itemTitleKey: 'title',
+      itemValueKey: 'value'
+    }),
+    
+    calendarSampleView: SC.View.design({
+      classNames: ['calendar-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    autoSuggestSampleView: SC.View.design({
+      classNames: ['auto-suggest-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    collapsibleSampleView: SC.View.design({
+      classNames: ['auto-suggest-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    contextMenuSampleView: SC.View.design({
+      classNames: ['context-menu-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    contentEditableSampleView: SC.View.design({
+      classNames: ['content-editable-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    uploadSampleView: SC.View.design({
+      classNames: ['upload-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
+    }),
+    
+    mixinSampleView: SC.View.design({
+      classNames: ['mixin-sample'],
+      layout: { top: 30, left: 0, right: 0, bottom: 0 }
     })
   })
 
