17d96be4674867511a131f1d65d701c6d3e543e5
Evin Grano <etgryphon@cox.net>
2009-10-06 20:23:59 -0400

    Initial import of the code

---
+++ Buildfile
@@ -0,0 +1,7 @@
+# ===========================================================================
+# Project:   Scui-sampleapp/
+# Copyright: ©2009 My Company, Inc.
+# ===========================================================================
+
+# Add initial buildfile information here
+config :all, :required => :sproutcore
---
+++ apps/samples/main.js
@@ -0,0 +1,30 @@
+// ==========================================================================
+// Project:   Samples
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals Samples */
+
+// This is the function that will start your app running.  The default
+// implementation will load any fixtures you have created then instantiate
+// your controllers and awake the elements on your page.
+//
+// As you develop your application you will probably want to override this.
+// See comments for some pointers on what to do next.
+//
+Samples.main = function main() {
+
+  // Step 1: Instantiate Your Views
+  // The default code here will make the mainPane for your application visible
+  // on screen.  If you app gets any level of complexity, you will probably 
+  // create multiple pages and panes.  
+  Samples.getPath('mainPage.mainPane').append() ;
+
+  // Step 2. Set the content property on your primary controller.
+  // This will make your app come alive!
+
+  // TODO: Set the content property on your primary controller
+  // ex: Samples.contactsController.set('content',Samples.contacts);
+
+} ;
+
+function main() { Samples.main(); }
