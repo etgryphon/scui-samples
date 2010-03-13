5a1aac372d309a12fb8c0d3c8749883655089e3c
Evin Grano <etgryphon@cox.net>
2010-03-13 07:59:47 -0500

    added the beginnings of code highlighting

--- apps/samples/views/code.js
+++ apps/samples/views/code.js
@@ -2,7 +2,7 @@
 // Project:   SCUI Samples - CodeView
 // Copyright: ©2009-2010 Evin Grano and Contributors
 // ==========================================================================
-/*globals Samples */
+/*globals Samples sh_highlightDocument  */
 
 Samples.CodeView = SC.View.extend({
   
@@ -18,6 +18,23 @@ Samples.CodeView = SC.View.extend({
           .text(code)
         .end();
     }
+  },
+  
+  isVisibleInWindow: function(){
+    var items = document.getElementsByTagName("pre");
+    for (var i=0, l = items.length; i < l; i++) {
+      var code = items[i].innerHTML;
+      console.log(code);
+      var match = code.match(/^<code>\#\!([a-z]+)\n/);
+      console.log(match);
+      if (match) {
+        items[i].className = "sh_" + match[1];
+        items[i].innerHTML = "<code>" + code.substr(match[0].length);
+      } else {
+        items[i].className = "sh_javascript";
+      }
+    }
+    sh_highlightDocument();
   }
   
 });
